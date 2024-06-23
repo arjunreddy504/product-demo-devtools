6e47e30b149f348ce52cf2ede1b40f5a5ea0c06d
ymreddy941 <ymreddy941@gmail.com>
2024-06-23 16:46:15 +0530

    first commit

---
+++ src/main/java/com/infy/productdemo/entity/Product.java
@@ -0,0 +1,23 @@
+package com.infy.productdemo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String type;
+    private double price;
+    
+}
